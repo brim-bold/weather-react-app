44e6e4e29adf9dfd9d34f98977ac11f5dc21ae41
brim-bold <87397805+brim-bold@users.noreply.github.com>
2023-02-07 20:23:28 -0500

    added rel="noreferrer"  to the links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
@@ -7,17 +7,18 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://www.brimandbold.com/" 
-        target="_blank">Jaylene Jeanpierre </a> and is{" "}
-        {""}
+        <a href="https://www.brimandbold.com/" target="_blank" rel="noreferrer">
+          Jaylene Jeanpierre{" "}
+        </a>{" "}
+        and is {""}
         <a
           href="https://github.com/brim-bold/weather-react-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
       </footer>
     </div>
   );
-  }
-
+}

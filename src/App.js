a8b1bd2a1a88ae54d34aa312ae386e67d7e3fd31
brim-bold <87397805+brim-bold@users.noreply.github.com>
2023-02-07 20:19:13 -0500

    added GitHUb repo URL

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App
-       </div>;
-  
-}
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.brimandbold.com/" 
+        target="_blank">Jaylene Jeanpierre </a> and is{" "}
+        {""}
+        <a
+          href="https://github.com/brim-bold/weather-react-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
+  }
 

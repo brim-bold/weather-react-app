ebbce41bde84567a2664a47269e1564487939df8
brim-bold <87397805+brim-bold@users.noreply.github.com>
2023-02-14 00:25:46 -0500

    added css to the App.css folder, fixed a bootstrap issue

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,16 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
-            <input type="submit" value="Search" className="btn btn-primary" />
           </div>
-          <div className="col-3"></div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
         </div>
       </form>
       <h1>New York</h1>
@@ -26,15 +32,16 @@ export default function Weather() {
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="mostly cloudy"
+            alt="Mostly Cloudy"
           />
-          6°C
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
             <li>Precipitation: 15%</li>
             <li>Humidity: 72%</li>
-            <li>Wind: 13 km/h%</li>
+            <li>Wind: 13 km/h</li>
           </ul>
         </div>
       </div>
